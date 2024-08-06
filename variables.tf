2b62cacbbe0b9f55bef1b4465a5f9a4b3b573fed
Robin Opletal <me@robinopletal.com>
2024-08-06 12:00:30 +0200

    fix(variables): empty defaults for labels and annotations

--- variables.tf
+++ variables.tf
@@ -6,9 +6,11 @@ variable "name" {
 variable "labels" {
   type        = map(string)
   description = "Labels to add to the namespace"
+  default     = {}
 }
 
 variable "annotations" {
   type        = map(string)
   description = "Annotations to add to the namespace"
+  default     = {}
 }
