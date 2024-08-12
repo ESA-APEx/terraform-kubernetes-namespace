6911942a27e458313c946d0d98b0d1458e9122a0
Robin Opletal <me@robinopletal.com>
2024-08-12 10:09:14 +0200

    feat(labels): add default labels and istio ambient label

--- namespace.tf
+++ namespace.tf
@@ -1,7 +1,14 @@
+locals {
+  default_labels = {
+    "istio.io/dataplane-mode" = "ambient"
+  }
+  final_labels = merge(local.default_labels, var.labels)
+}
+
 resource "kubernetes_namespace_v1" "namespace" {
   metadata {
     name        = var.name
-    labels      = var.labels
+    labels      = local.final_labels
     annotations = var.annotations
   }
 }
